da0f4f55d0714cbaeecdecba70a3be14ef28cb2d
amaurya <aakash.maurya@gap.com>
2022-12-03 22:10:38 +0530

    Array Subsets

---
+++ src/Arrays/ArraySubsets2.java
@@ -0,0 +1,168 @@
+package Arrays;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.InputMismatchException;
+
+public class ArraySubsets2 {
+
+    public static void main(String[] args) {
+
+        InputReader in = new InputReader(System.in);
+        int tc = in.nextInt();
+        for (int t = 0; t < tc; t++) {
+
+            int n = in.nextInt();
+            ArrayList<Integer> input = new ArrayList<>();
+            for(int i = 0;i<in.nextIntArray(n).length;i++){
+                input.add(i);
+            }
+            List<List<Integer>> ans = new ArrayList<>();
+            subsets(input, input, 1, input.size(), ans);
+            for (List<Integer> i:ans) {
+                for (int s:i) {
+                    System.out.print(s + " ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+    private static List<List<Integer>> subsets(List<Integer> sublist, List<Integer> input, int idx, int size, List<List<Integer>> ans){
+        if(idx == size + 1)
+            return ans;
+
+        List<Integer> sub = new ArrayList<>();
+        for (Integer integer : sublist) {
+            sub.add(integer);
+            ans.addAll(Collections.singleton(new ArrayList<Integer>(sub)));
+        }
+
+        ArrayList<Integer>  subList = new ArrayList<Integer>(input.subList(idx, size));
+        return subsets(subList ,input,idx+1, size, ans);
+    }
+
+    private static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+
+    }
+
+}
